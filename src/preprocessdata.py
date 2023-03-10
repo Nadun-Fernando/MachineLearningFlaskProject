1efbedab55b64a81967b1622777f5c9aa3d4e81b
Nadun-Fernando <nadunnilupul@gmail.com>
2023-03-10 06:08:34 +0530

    updated preprocessing

--- src/preprocessdata.py
+++ src/preprocessdata.py
@@ -1,26 +1,32 @@
 import pandas as pd
-import numpy as np
 import re
-import nltk
 from nltk.corpus import stopwords
 
 
 class PreprocessData:
     __df = pd.DataFrame
 
+    def __init__(self, df):
+        self.__df = self.__clean_texts(df)
+
     def __clean_texts(self, df):
         df['text'] = df['text'].str.lower()
 
-        ## remove html tags if there are any
+        # remove html tags if there are any
         r = re.compile(r'<[^>]+')
         df['text'] = [r.sub('', s) for s in df['text'].tolist()]
 
-        ## remove everthing except letters
+        # remove everthing except letters
         r = re.compile(r'[^\w\s]+')
         df['text'] = [r.sub('', s) for s in df['text'].tolist()]
 
-        ## remove stopword from the text
+        # remove stopword from the text
         r = re.compile(r'\b(' + r'|'.join(stopwords.words('english')) + r')\b\s*')
         df['text'] = [r.sub('', s) for s in df['text'].tolist()]
 
+        # remove one word sentences
+        df = df[[len(i.split()) > 1 for i in df['text'].values]]
+
         return df
+
+
