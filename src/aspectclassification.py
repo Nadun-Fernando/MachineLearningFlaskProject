4952f3d1533f238ea88ee25fa7d6d03a98ec08ff
Nadun-Fernando <nadunnilupul@gmail.com>
2023-03-12 03:35:27 +0530

    updated category predictions

--- src/aspectclassification.py
+++ src/aspectclassification.py
@@ -4,14 +4,8 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 import joblib
-from nltk.stem import WordNetLemmatizer
-
-import re
-import nltk
 
 from src.preprocessdata import *
-
-from nltk.corpus import stopwords
 
 
 class AspectClassification:
@@ -23,6 +17,30 @@ class AspectClassification:
         self.__df = pd.read_csv('./data/scrapped.csv')
         processed = PreprocessData(self.__df)
         self.__df = processed.getdataframe()
-        print(self.__df)
+        category_predictions = self.__predictaspect()
+        self.__savetocsv(category_predictions)
+        # print(self.__df)
 
+    def __predictaspect(self):
+        data = list(self.__df['lemmatized'])
+        # unseen_reviews
+
+        data_transformed = self.__vectorizer.transform(data)
+
+        # Passing tokenised instance to the model for predictions
+        pred = self.__clf.predict(data_transformed)
+
+        return pred
+
+    def __savetocsv(self, pred):
+        self.__df['category'] = pred
+
+        prediction_category = pd.DataFrame(self.__df['category'], columns=['category'])
+        rating = pd.DataFrame(self.__df['rating'], columns=['rating'])
+        text = pd.DataFrame(self.__df['text'], columns=['text'])
+
+        dfx = pd.concat([rating, text, prediction_category], axis=1)
+
+        dfx.to_csv("./data/scrapped_category_predicted.csv")
+        print(dfx)
 
